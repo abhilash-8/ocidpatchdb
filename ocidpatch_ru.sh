1710b1101f205b0fbc3e303282124014c7cb0494
Abhilash Kumar Bhattaram <abhilash8@gmail.com>
2024-06-30 22:10:22 +0530

    Update ocidpatch_ru.sh

--- ocidpatch_ru.sh
+++ ocidpatch_ru.sh
@@ -24,26 +24,26 @@ call_ru()
         echo $ACTION
         echo " "
         echo "### DB System Summary "
-        oci db system list -c $COMP_OCID --profile SHAI-PROFILE --query "data[?contains(\"hostname\",'$HOSTNAME_IN_OCI')].{hostname:hostname,\"database-edition\":\"database-edition\",dbsysversion:version,id:id}" --output table
+        oci db system list -c $COMP_OCID --profile MY-PROFILE --query "data[?contains(\"hostname\",'$HOSTNAME_IN_OCI')].{hostname:hostname,\"database-edition\":\"database-edition\",dbsysversion:version,id:id}" --output table
         echo " "
         echo "### OCID for DB System to run Patch/PreRequisites"
-        export DB_SYSTEM_OCID=$(oci db system list -c $COMP_OCID --profile SHAI-PROFILE --query "data[?contains(\"hostname\",'$HOSTNAME_IN_OCI')].{id:id}" |  jq -r '.[]."id"');echo $DB_SYSTEM_OCID
+        export DB_SYSTEM_OCID=$(oci db system list -c $COMP_OCID --profile MY-PROFILE --query "data[?contains(\"hostname\",'$HOSTNAME_IN_OCI')].{id:id}" |  jq -r '.[]."id"');echo $DB_SYSTEM_OCID
         echo " "
         echo "### OCID for Database"
-        #oci db database list --compartment-id $COMP_OCID --profile SHAI-PROFILE --output table  
-        oci db database list --compartment-id $COMP_OCID --profile SHAI-PROFILE --query "data[?contains(\"db-name\",'$DBNAME_IN_OCI')].{\"db-name\":\"db-name\",\"db-unique-name\":\"db-unique-name\",id:id}" --output table
+        #oci db database list --compartment-id $COMP_OCID --profile MY-PROFILE --output table  
+        oci db database list --compartment-id $COMP_OCID --profile MY-PROFILE --query "data[?contains(\"db-name\",'$DBNAME_IN_OCI')].{\"db-name\":\"db-name\",\"db-unique-name\":\"db-unique-name\",id:id}" --output table
         echo " "        
         echo "### OCID for Database to Patch/PreRequisites"     
-        export DB_OCID=$(oci db database list --compartment-id $COMP_OCID --profile SHAI-PROFILE --query "data[?contains(\"db-name\",'$DBNAME_IN_OCI')]" |  jq -r '.[]."id"');echo $DB_OCID
+        export DB_OCID=$(oci db database list --compartment-id $COMP_OCID --profile MY-PROFILE --query "data[?contains(\"db-name\",'$DBNAME_IN_OCI')]" |  jq -r '.[]."id"');echo $DB_OCID
         echo " "
         echo "### OCID for Databse RU Patches which are available"
-        oci db patch list by-database --database-id $DB_OCID --profile SHAI-PROFILE --all #--output table
+        oci db patch list by-database --database-id $DB_OCID --profile MY-PROFILE --all #--output table
         echo " "
         echo "### OCID for DB RU Patch"
-        export PATCH_OCID=$(oci db patch list by-database --database-id $DB_OCID --profile SHAI-PROFILE --all --query "data[?contains(\"version\",'$VERSION_TO_APPLY')]" |  jq -r '.[]."id"');echo $PATCH_OCID
+        export PATCH_OCID=$(oci db patch list by-database --database-id $DB_OCID --profile MY-PROFILE --all --query "data[?contains(\"version\",'$VERSION_TO_APPLY')]" |  jq -r '.[]."id"');echo $PATCH_OCID
         echo " "
         echo "### DB RU Apply Action " $ACTION
-        oci db database patch --database-id $DB_OCID --patch-id $PATCH_OCID --patch-action $ACTION  --profile SHAI-PROFILE
+        oci db database patch --database-id $DB_OCID --patch-id $PATCH_OCID --patch-action $ACTION  --profile MY-PROFILE
 }
 ## Main
 if [ -z "$5" ]
