1897c7bbf4b93d4ca4a0d190a4b75590b765b7b8
Abhilash Kumar Bhattaram <abhilash8@gmail.com>
2024-07-04 21:53:14 +0530

    Update ocidpatch_ru.sh

--- ocidpatch_ru.sh
+++ ocidpatch_ru.sh
@@ -21,43 +21,44 @@ call_ru()
         echo $DBENV
         echo $HOSTNAME_IN_OCI
         echo $DBNAME_IN_OCI
+        echo $VERSION_TO_APPLY
         echo $ACTION
         echo " "
         echo "### DB System Summary "
-        oci db system list -c $COMP_OCID --profile MY-PROFILE --query "data[?contains(\"hostname\",'$HOSTNAME_IN_OCI')].{hostname:hostname,\"database-edition\":\"database-edition\",dbsysversion:version,id:id}" --output table
+        oci db system list -c $COMP_OCID --profile SHAI-PROFILE --query "data[?contains(\"hostname\",'$HOSTNAME_IN_OCI')].{hostname:hostname,\"database-edition\":\"database-edition\",dbsysversion:version,id:id}" --output table
         echo " "
         echo "### OCID for DB System to run Patch/PreRequisites"
-        export DB_SYSTEM_OCID=$(oci db system list -c $COMP_OCID --profile MY-PROFILE --query "data[?contains(\"hostname\",'$HOSTNAME_IN_OCI')].{id:id}" |  jq -r '.[]."id"');echo $DB_SYSTEM_OCID
+        export DB_SYSTEM_OCID=$(oci db system list -c $COMP_OCID --profile SHAI-PROFILE --query "data[?contains(\"hostname\",'$HOSTNAME_IN_OCI')].{id:id}" |  jq -r '.[]."id"');echo $DB_SYSTEM_OCID
         echo " "
         echo "### OCID for Database"
-        #oci db database list --compartment-id $COMP_OCID --profile MY-PROFILE --output table  
-        oci db database list --compartment-id $COMP_OCID --profile MY-PROFILE --query "data[?contains(\"db-name\",'$DBNAME_IN_OCI')].{\"db-name\":\"db-name\",\"db-unique-name\":\"db-unique-name\",id:id}" --output table
+        #oci db database list --compartment-id $COMP_OCID --profile SHAI-PROFILE --output table  
+        oci db database list --compartment-id $COMP_OCID --profile SHAI-PROFILE --query "data[?contains(\"db-name\",'$DBNAME_IN_OCI')].{\"db-name\":\"db-name\",\"db-unique-name\":\"db-unique-name\",id:id}" --output table
         echo " "        
         echo "### OCID for Database to Patch/PreRequisites"     
-        export DB_OCID=$(oci db database list --compartment-id $COMP_OCID --profile MY-PROFILE --query "data[?contains(\"db-name\",'$DBNAME_IN_OCI')]" |  jq -r '.[]."id"');echo $DB_OCID
+        export DB_OCID=$(oci db database list --compartment-id $COMP_OCID --profile SHAI-PROFILE --query "data[?contains(\"db-name\",'$DBNAME_IN_OCI')]" |  jq -r '.[]."id"');echo $DB_OCID
         echo " "
         echo "### OCID for Databse RU Patches which are available"
-        oci db patch list by-database --database-id $DB_OCID --profile MY-PROFILE --all #--output table
+        oci db patch list by-database --database-id $DB_OCID --profile SHAI-PROFILE --all #--output table
         echo " "
         echo "### OCID for DB RU Patch"
-        export PATCH_OCID=$(oci db patch list by-database --database-id $DB_OCID --profile MY-PROFILE --all --query "data[?contains(\"version\",'$VERSION_TO_APPLY')]" |  jq -r '.[]."id"');echo $PATCH_OCID
+        export PATCH_OCID=$(oci db patch list by-database --database-id $DB_OCID --profile SHAI-PROFILE --all --query "data[?contains(\"version\",'$VERSION_TO_APPLY')]" |  jq -r '.[]."id"');echo $PATCH_OCID
         echo " "
         echo "### DB RU Apply Action " $ACTION
-        oci db database patch --database-id $DB_OCID --patch-id $PATCH_OCID --patch-action $ACTION  --profile MY-PROFILE
+        oci db database patch --database-id $DB_OCID --patch-id $PATCH_OCID --patch-action $ACTION  --profile SHAI-PROFILE
 }
 ## Main
 if [ -z "$5" ]
 then
         echo " "        
-        echo "######################################################################################### "       
-        echo "Usage is ocidpatch_ru.sh <compartment_name> <hostname_in_oci> <dbname> <action>"
+        echo "#################################################################################################### "       
+        echo "Usage is ocidpatch_ru.sh <compartment_name> <hostname_in_oci> <dbname> <version_needed> <action>"
         echo " "        
-        echo "E.g.  # ./ocidpatch_ru.sh DEV devdb01 orcl 19.XX.0.0.0 PRECHECK "
-        echo "E.g.  # ./ocidpatch_ru.sh DEV devdb01 orcl 19.XX.0.0.0 APPLY "
+        echo "E.g.  # sh ocidpatch_ru.sh DEV devdb01 orcl 19.XX.0.0.0 PRECHECK "
+        echo "E.g.  # sh ocidpatch_ru.sh DEV devdb01 orcl 19.XX.0.0.0 APPLY "
         echo " "        
         echo "Pre-requisites : ocidtab environment variable files to be available  "    
         echo "               : Refer https://github.com/abhilash-8/ocidenv to generate ocidtab file  "  
-        echo "######################################################################################### "       
+        echo "#################################################################################################### "       
         echo " " 
 else
         call_ru
